fd2c58b463a55b4795f1683161b22ef21c443233
Craig-WIT <20095351@mail.wit.ie>
2024-12-07 21:52:06 +0000

    Creating a flexible Button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,3 +1,39 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface BasicProps {
+    children: Snippet;
+    isSecondary?: boolean;
+    isDangerous?: boolean;
+    isMenu?: boolean;
+  }
+
+  interface ButtonProps extends BasicProps {
+    onclick: (e:MouseEvent) => void;
+    href?: never;
+  }
+
+  interface LinkProps extends BasicProps {
+    href: string;
+    onclick?: never;
+  }
+
+  type ComponentProps = ButtonProps | LinkProps
+
+  let { children, href, onclick, isSecondary, isDangerous, isMenu, ...props }: ComponentProps = $props();
+</script>
+
+{#if href}
+  <a {href} class="btn" class:btn-secondary={isSecondary} class:btn-danger={isDangerous} class:btn-menu={isMenu}>
+    {@render children()}
+  </a>
+
+{:else}
+  <button {...props} {onclick} class="btn" class:btn-secondary={isSecondary} class:btn-danger={isDangerous} class:btn-menu={isMenu}>
+    {@render children()}
+  </button>
+{/if}
+
 <style>
   a {
     display: block;
