a527f667f9c0180883f2c53fa6f48333f861eb93
Craig-WIT <20095351@mail.wit.ie>
2024-12-07 12:50:32 +0000

    Creating landing page for application

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,3 +1,22 @@
+<script>
+  import heroImage from "$assets/hero.png"
+</script>
+
+<section class="default-margin hero">
+  <div class="hero-text">
+    <h1>Book Nest</h1>
+    <h3>Your personal book library</h3>
+    <h4 class="mt-l">
+      Create your very own digital book library
+    </h4>
+    <h4 class="mb-s">
+      Our app makes managing your connection a joy
+    </h4>
+    <button>Sign Up</button>
+  </div>
+  <img src="{heroImage}" alt="" class="hero-image">
+</section>
+
 <style>
   .hero {
     display: flex;
