1a4bde5bcd88b7ac1e54a06f738c961bfed756a9
Craig-WIT <20095351@mail.wit.ie>
2024-12-13 21:45:42 +0000

    Creating a BookCategory component

--- src/lib/components/BookCategory.svelte
+++ src/lib/components/BookCategory.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+  import type { Book } from "$lib/state/user-state.svelte";
+  import BookCard from "./BookCard.svelte";
+
+  interface BookCategoryProps {
+    categoryName: String;
+    booksToDisplay: Book[];
+  }
+
+  let { categoryName, booksToDisplay } : BookCategoryProps = $props();
+</script>
+
+<section class="books-category mb-m">
+  <h3 class="mb-s">{categoryName}</h3>
+  <div class="books-container">
+    {#each booksToDisplay as book}
+      <BookCard {book}></BookCard>
+    {/each}
+  </div>
+</section>
+
 <style>
   .books-container {
     display: flex;
@@ -13,6 +34,6 @@
 
   .books-container::webkit-scrollbar-thumb {
     background-color: rgba(0, 0, 0, 0.2);
-    border-radius: 3px;
+    border-radius: 10px;
   }
 </style>
--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { BookCard } from "$components";
+  import BookCategory from "$components/BookCategory.svelte";
   import { getUserState } from "$lib/state/user-state.svelte";
   import Icon from "@iconify/svelte";
 
@@ -24,9 +25,10 @@
       <p>Have you discovered any new favourites recently?</p>
     </div>
   </div>
+  <BookCategory booksToDisplay={allBooks.slice(0,10)} categoryName={"Favourites"}></BookCategory>
 </div>
 {#each allBooks as book}
-<BookCard {book}/>
+  <BookCard {book}/>
 {/each}
 
 <style>
