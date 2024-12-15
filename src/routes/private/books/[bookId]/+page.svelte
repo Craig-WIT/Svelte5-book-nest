9a78cc49c1045a08838e9ee564c9a95cfbcbb796
Craig-WIT <20095351@mail.wit.ie>
2024-12-15 14:08:51 +0000

    Addng individual book details page

--- src/routes/private/books/[bookId]/+page.svelte
+++ src/routes/private/books/[bookId]/+page.svelte
@@ -1,3 +1,77 @@
+<script lang="ts">
+  import Button from "$components/Button.svelte";
+  import StarRating from "$components/StarRating.svelte";
+  import type { Book } from "$lib/state/user-state.svelte";
+  import Icon from "@iconify/svelte";
+
+  interface bookPageProps {
+    data: {
+      book: Book;
+    }
+  }
+
+  let {data}: bookPageProps = $props()
+
+  let book = $derived(data.book)
+
+  function goBack() {
+    history.back();
+}
+  
+</script>
+
+{#snippet bookInfo()}
+
+<h2 class="book-title mb-m">{book.title}</h2>
+<p class="book-author">by {book.author}</p>
+<h4 class="mt-m mb-xs semi-bold">Your rating</h4>
+<StarRating value={book.rating || 0} isReadOnly={false}></StarRating>
+<p class="small-font">
+  Click to {book.rating ? "change" : "give"} rating
+</p>
+{#if book.description}
+  <h4 class="mt-m mb-xs semi-bold">Description</h4>
+  <p class="mb-m">{book.description}</p>
+{:else}
+<h4 class="mt-m mb-xs semi-bold">No description yet</h4>
+<button class="block mb-m" onclick={() => console.log("Toggle edit mode")}>
+  <p>Click to add description</p>
+</button>
+{/if}
+
+{#if !book.finished_reading_on}
+  <Button isSecondary={true} onclick={() => console.log("Update reading status")}>
+    {book.started_reading_on ? "I finished reading this book" : "I started reading this book"}
+  </Button>
+{/if}
+{#if book.genre}
+<h4 class="mt-m mb-xs semi-bold">Genre</h4>
+<p>{book.genre}</p>
+{/if}
+  
+{/snippet}
+
+<div class="bookpage">
+  <button onclick={goBack} aria-label="Go back">
+    <Icon icon="ep:back" width={"40"} />
+  </button>
+  <div class="book-container">
+    <div class="book-info">
+      {@render bookInfo()}
+    </div>
+    <div class="book-cover">
+      {#if book.cover_image}
+        <img src={book.cover_image} alt="Book cover">
+      {:else}
+        <button class="add-cover">
+          <Icon class="add-image-icon" icon="bi:camera-fill" width={"40"} />
+          <p>Add book cover</p>
+        </button>
+      {/if}
+    </div>
+  </div>
+</div>
+
 <style>
   .book-container {
     display: flex;
